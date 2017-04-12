caa3ae2ea02f8454d2a94e77d7596ac8e983c7b6
xli <xli@kt360.org>
2017-04-12 11:01:45 +0800

    aaaa

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -1,9 +1,12 @@
+import org.json.JSONObject;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("11111111");
+		JSONObject obj = new JSONObject();
+		obj.put("bbb", "aaa");
+		System.out.println("11111111" + obj);
 	}
 
 }
